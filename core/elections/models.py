a1bcfd1d633320eaf0585c36351de98d5be20a5a
gerardzikpi <kodinapetra@yahoo.com>
2026-06-24 19:46:02 -0700

    added the Votes and Candidates models

--- core/elections/models.py
+++ core/elections/models.py
@@ -1,6 +1,59 @@
 from django.db import models
 import uuid
 # Create your models here.
+from voters.models import ElectionVoter
+
+POSITION_CHOICES = [
+    ("SRC_PRESIDENT","SRC President"),
+    ("SRC_VICE","SRC Vice President"),
+    ("SRC_SECRETARY","SRC Secretary"),
+    ("SRC_ORGANISER","SRC Orangiser"),
+    ("SRC_TREASURER","SRC Treasurer"),
+]
+
+class Votes(models.Model):
+    "A submodel to represent the votes cast in an election."
+    
+    election = models.ForeignKey("Election", on_delete=models.CASCADE)
+    votes = models.IntegerField(auto_created=True,default=0)
+    voter = models.ForeignKey(ElectionVoter,on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name_plural = "Votes"
+
+    def __str__(self):
+        return f'Ballot {self.election_id}'
+    
+    def save(self, force_insert = ..., force_update = ..., using = ..., update_fields = ...):
+        if self.pk:
+            old = Votes.objects.get(pk=self.pk)
+            if old.voter != self.voter:
+                raise ValueError(
+                    "Voter cannot be changed once a vote is cast"
+                )
+            
+        if self.voter.election != self.election:
+            raise ValueError("Voter does not belong to this election")
+
+        if self.voter:
+            self.votes += 1
+
+        if self.votes< 0:
+            raise ValueError("Votes cannot be negative")
+
+        return super().save(force_insert, force_update, using, update_fields)  
+
+
+class Candidate(models.Model):
+    """Candidate to be elected."""
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to="photos")
+    position = models.CharField(max_length=20,choices=POSITION_CHOICES)
+    votes = models.ForeignKey("Votes", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Candidate{self.name}'
 
 class ElectionStatus(models.TextChoices):
     DRAFT = "DRAFT", "Draft",
@@ -16,8 +69,7 @@ class Election(models.Model):
         editable=False 
     )
     
-    title = models.CharField(max_length=
-                             255)
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=ElectionStatus.choices, default=ElectionStatus.DRAFT)
     start_date = models.DateTimeField()
@@ -25,6 +77,7 @@ class Election(models.Model):
     is_locked =models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
+    candidate = models.ForeignKey(Candidate,on_delete=models.CASCADE,default = None)
     
     
     def save(self, *args, **kwargs):
@@ -75,4 +128,5 @@ class Election(models.Model):
         
     def __str__(self):
         return f"{self.title} ({self.status})"
-        
+
+ 
